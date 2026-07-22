7bf54d96cf425b154fca7f53e79aff0b5e274f4d
Kartikeya <kartikeya.anjul@gmail.com>
2026-07-22 15:12:36 +0530

    import service

--- apps/api/src/controllers/job.controller.ts
+++ apps/api/src/controllers/job.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
+import { JobService } from "../services/job.service.js";
+const jobService = new JobService();
 
 export async function createJob(_req: Request, res: Response) {
+    await jobService.createJob();
     return res.status(200).json({
         message: "Controller reached",
     });
