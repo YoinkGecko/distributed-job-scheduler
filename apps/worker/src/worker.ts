552efb4b3021e5a8b75c4e1fb864fd782736af02
Kartikeya <kartikeya.anjul@gmail.com>
2026-07-23 00:32:57 +0530

    created worker

--- apps/worker/src/worker.ts
+++ apps/worker/src/worker.ts
@@ -1,7 +1,30 @@
 import redis from "./redis/client";
 
 async function startWorker() {
+  try {
+    await redis.xgroup("CREATE", "jobs-stream", "workers", "0", "MKSTREAM");
 
+    console.log("Consumer Group Created");
+  } catch (err) {
+    console.log("Consumer Group already exists");
+  }
+
+  while (true) {
+    const jobs = await redis.xreadgroup(
+      "GROUP",
+      "workers",
+      "worker-1",
+      "BLOCK",
+      0,
+      "COUNT",
+      1,
+      "STREAMS",
+      "jobs-stream",
+      ">",
+    );
+
+    console.log(jobs);
+  }
 }
 
 startWorker();
