0219bf4730e1ca2dd6c323424b1420d15af4d069
Kartikeya <kartikeya.anjul@gmail.com>
2026-07-23 15:18:09 +0530

    c

--- packages/redis/src/client.ts
+++ packages/redis/src/client.ts
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || "localhost",
-  port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
+  host: process.env.REDIS_HOST ,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
   maxRetriesPerRequest: null,
 });
 
