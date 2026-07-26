2d2b2aad3f9b6cb62f2142c4d94e56b11713e183
Kartikeya <kartikeya.anjul@gmail.com>
2026-07-26 17:50:07 +0530

    feat(outbox-publisher) added outbox repository to interact with DB

---
+++ apps/outbox-publisher/src/repository/OutboxRepository.ts
@@ -0,0 +1,32 @@
+import { OutboxEvent, JobCreatedEventPayload } from "@scheduler/types";
+
+import { pool } from "@scheduler/database";
+
+export class OutboxRepository {
+
+  async findUnpublishedEvents(limit: number): Promise<OutboxEvent<JobCreatedEventPayload>[]> {
+    const query = `
+      SELECT *
+      FROM outbox_events
+      WHERE published = FALSE
+      ORDER BY created_at
+      LIMIT $1;
+    `;
+
+    const result = await pool.query(query, [limit]);
+
+    return result.rows;
+  }
+
+  async markPublished(eventId: string): Promise<void> {
+    const query = `
+      UPDATE outbox_events
+      SET
+        published = TRUE,
+        published_at = NOW()
+      WHERE id = $1;
+    `;
+
+    await pool.query(query, [eventId]);
+  }
+}
